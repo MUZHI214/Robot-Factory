72fb3cc8fee53e644dfeec2408e28d035829a56c
MajesticalMelon <owenfriend2002@gmail.com>
2024-04-15 11:25:40 -0400

    Make Robot an extension of Entity

--- Robot-Factory/Assets/Scripts/Character/Robot.cs
+++ Robot-Factory/Assets/Scripts/Character/Robot.cs
@@ -2,21 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Robot : MonoBehaviour
+public class Robot : Entity
 {
     private int currentPathIndex;
     private List<Vector3> pathVectorList;
-    public float speed = 40f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
+        base.Update();
+
         Movement();
     }
 
